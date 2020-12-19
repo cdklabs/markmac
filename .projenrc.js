fdce409612b367381e02660259b38881a396248a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-19 10:05:37 -0500

    chore(deps): bumps projen from 0.3.99 to 0.6.16

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,16 @@
 const { TypeScriptProject } = require('projen');
 
 const project = new TypeScriptProject({
-  name: "markmac",
+  name: 'markmac',
   releaseToNpm: true,
-  authorName: "Elad Ben-Israel",
-  repository: "https://github.com/eladb/markmac.git",
-  authorEmail: "elad.benisrael@gmail.com",
+  authorName: 'Elad Ben-Israel',
+  repository: 'https://github.com/eladb/markmac.git',
+  authorEmail: 'elad.benisrael@gmail.com',
   bin: {
-    'markmac': 'lib/cli.js'
+    markmac: 'lib/cli.js',
   },
   devDeps: [
-    'memory-streams'
+    'memory-streams',
   ],
 });
 
