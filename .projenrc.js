47eff25d6b1a598372fd9d2a446da3887689bb47
Your Name <you@example.com>
2021-06-13 22:15:31 +0300

    upgrade projen + auto approve

--- .projenrc.js
+++ .projenrc.js
@@ -1,17 +1,24 @@
 const { TypeScriptProject } = require('projen');
 
 const project = new TypeScriptProject({
-  name: "markmac",
+  name: 'markmac',
   releaseToNpm: true,
-  authorName: "Elad Ben-Israel",
-  repository: "https://github.com/eladb/markmac.git",
-  authorEmail: "elad.benisrael@gmail.com",
+  authorName: 'Elad Ben-Israel',
+  repository: 'https://github.com/eladb/markmac.git',
+  authorEmail: 'elad.benisrael@gmail.com',
   bin: {
-    'markmac': 'lib/cli.js'
+    markmac: 'lib/cli.js',
   },
   devDeps: [
-    'memory-streams'
+    'memory-streams',
   ],
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  autoApproveOptions: {
+    allowedUsernames: ['aws-cdk-automation'],
+    secret: 'GITHUB_TOKEN',
+  },
+  autoApproveUpgrades: true,
+  defaultReleaseBranch: 'master',
 });
 
 project.synth();
